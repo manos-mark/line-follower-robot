2f062fceda167667b604eed8c466e4d88b6efbe0
manos <manos-mark@hotmail.com>
2021-05-28 12:28:40 +0300

    Data Collection Module

---
+++ MainDataCollectionModule.py
@@ -0,0 +1,67 @@
+import WebcamModule as wM
+import DataCollectionModule as dcM
+import KeyboardControlModule as kcM
+import MotorModule as mM
+import cv2 as cv
+from time import sleep
+
+
+maxThrottle = 0.25
+
+##################################
+motor = mM.Motor(5, 22, 23, 6, 24, 25)
+##################################
+
+MAX_SPEED = 0.7
+MIN_SPEED = 0.6
+RIGHT_TURN = -1
+LEFT_TURN = 1
+
+DELAY = 0.1
+
+recording = False
+
+
+def record(turn):
+    if recording:
+        img = wM.getImg(True, size=[240, 120])
+        dcM.saveData(img, turn)
+    elif not recording:
+        dcM.saveLog()
+
+
+while True:
+
+    ####### CONTROL USING KEYBOARD
+    kcM.init()
+
+    if kcM.get_keyboard_input('R') or kcM.get_keyboard_input('r'):
+        if not recording:
+            print('Recording Started ...')
+            recording = True
+            sleep(0.300)
+        else:
+            print('Recording Finished ...')
+            recording = False
+            sleep(0.300)
+
+    if kcM.get_keyboard_input('LEFT'):
+        motor.move(MIN_SPEED, LEFT_TURN, DELAY)
+        record(LEFT_TURN)
+    elif kcM.get_keyboard_input('RIGHT'):
+        motor.move(MIN_SPEED, RIGHT_TURN, DELAY)
+        record(RIGHT_TURN)
+    elif kcM.get_keyboard_input('UP'):
+        motor.move(MAX_SPEED, 0, DELAY)
+        record()
+    elif kcM.get_keyboard_input('DOWN'):
+        motor.move(-MAX_SPEED, 0, DELAY)
+        record()
+    else:
+        motor.stop(0.1)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        GPIO.cleanup()
+        break
+
+    cv.destroyAllWindows()
